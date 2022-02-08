ebea444c8ae14b7b1618e922a3d7c48ffbfc934d
Pergon <pergonium@gmail.ocm>
2022-02-08 11:07:33 +0700

    new

--- Assembly/test.asm
+++ Assembly/test.asm
@@ -1,21 +1,8 @@
 section .data
-    text db "hello world!",10
+    text db 'Hello, World!', 10
 
 section .text
     global _start
 
 _start:
-    call _print
-
-_exit:
-    mov rax, 60
-    mov rdi, 0
-    syscall
-
-_print:
-    mov rax, 1 
-    mov rdi, 1
-    mov rsi, text
-    mov rdx, 13
-    syscall
-    ret
+    
